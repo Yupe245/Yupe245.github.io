cff14eef95426b395ff23fae049f39165f0b8a75
Yupe452 <69852657+Yupe245@users.noreply.github.com>
2023-02-13 23:06:10 +0300

    bug fix

--- snake.js
+++ snake.js
@@ -48,7 +48,7 @@ class Apple {
 
     draw() {
         const img = new Image();
-        img.src = "apple.png";
+        img.src = "http://localhost:8080/public/apple.png";
         ctx.drawImage(img, this.x + 5, this.y + 4);
     };
 };
@@ -168,23 +168,18 @@ function loop() {
         };
 
         if (state.snake.x < 0) {
-            state.snake.x = state.cols * state.size + state.size;
+            state.snake.x = state.cols * state.size;
         };
-        if (state.snake.x > state.cols * state.size + state.size) {
+        if (state.snake.x > state.cols * state.size) {
             state.snake.x = 0;
         };
 
         if (state.snake.y < 0) {
-            state.snake.y = state.rows * state.size + state.size;
+            state.snake.y = state.rows * state.size;
         };
-        if (state.snake.y > state.rows * state.size + state.size) {
+        if (state.snake.y > state.rows * state.size + 10) {
             state.snake.y = 0;
         };
-
-        console.log("snake", state.snake);
-
-        // gameOver();
-        //  ||  || state.snake.y < 0 || 
 
         if (state.gameOver) {
             new Text("Game Over!", canvas.width / 2, canvas.height / 2 - 20, "center", 50).draw();
